53b92408b1d9f762c6e4c9192911fd3a49f0acb1
info <info@hexgecko.com>
2021-05-11 10:50:45 +0200

    Update fixed center.

--- render/fixed_center.lua
+++ render/fixed_center.lua
@@ -1,6 +1,6 @@
 local M = {
 	__pick_matrix = vmath.matrix4(),
-	__world_to_gui_matrix = vmath.matrix4(),
+	__world_to_screen_matrix = vmath.matrix4(),
 	__viewport_rect,
 	__screen_rect,
 	__window_size,
@@ -15,13 +15,13 @@ function M.get_screen_rect()
 	return unpack(M.__screen_rect)
 end
 
-function M.window_to_world(x, y)
+function M.screen_to_world(x, y)
 	local p = M.__pick_matrix * vmath.vector4(x, y, 0, 1)
 	return p.x, p.y
 end
 
-function M.world_to_gui(x, y)
-	local p = M.__world_to_gui_matrix * vmath.vector4(x, y, 0, 1)
+function M.world_to_screen(x, y)
+	local p = M.__world_to_screen_matrix * vmath.vector4(x, y, 0, 1)
 	return p.x, p.y
 end
 
@@ -71,7 +71,7 @@ function M.__calc_matrix(proj, view, width, height)
 	n.m22 = 2
 	n.m23 = -1
 	M.__pick_matrix = vmath.inv(proj * view) * n
-	M.__world_to_gui_matrix = vmath.inv(M.__pick_matrix)
+	M.__world_to_screen_matrix = vmath.inv(M.__pick_matrix)
 end
 
 return M
